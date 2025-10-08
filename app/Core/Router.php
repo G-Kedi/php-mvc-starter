0aae94178889cf209e35b3de70025a03fdbcf7d0
G-kedi <gedeonkedi4@gmail.com>
2025-10-08 12:22:33 +0100

    feat: Add get() method to Router for registering GET routes

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Core;
+namespace Gkedi\PhpMvcStater;
 
 /**
  * Router class
@@ -8,10 +8,19 @@ namespace App\Core;
 class Router
 {
     private string $request;
+    private array $routes = [];
 
     public function __construct(string $request)
     {
         $this->request = $request;
         // For now, we just store the request
+    }
+
+    /**
+     * Register a GET route
+     */
+    public function get(string $path, string $action): void
+    {
+        $this->routes['GET'][$path] = $action;
     }
 }
